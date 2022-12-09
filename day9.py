cc6ff5bf3e7f4923118e06006a151dbf3fcbaf92
lhofmann <lhofmann@users.noreply.github.com>
2022-12-09 11:05:43 +0100

    Clean up code.

--- day9.py
+++ day9.py
@@ -2,24 +2,24 @@ DATA = open('day9.txt').read()
 
 
 def run(l=2):
-    head = [0, 0]
-    tail = [[0, 0] for _ in range(l - 1)]
-    visited = set([tuple(tail[-1])])
+    rope = [[0, 0] for _ in range(l)]
+    visited = set([tuple(rope[-1])])
 
     for direction, n in (line.split() for line in DATA.splitlines()):
         vx, vy = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0),
                   'R': (1, 0)}[direction]
         for _ in range(int(n)):
-            head[0] += vx
-            head[1] += vy
+            rope[0][0] += vx
+            rope[0][1] += vy
 
-            for p, q in zip([head] + tail[:-1], tail):
+            for p, q in zip(rope[:-1], rope[1:]):
                 dx, dy = p[0] - q[0], p[1] - q[1]
                 if abs(dx) <= 1 and abs(dy) <= 1:
                     continue
-                q[0] += max(-1, min(1, p[0] - q[0]))
-                q[1] += max(-1, min(1, p[1] - q[1]))
-            visited.add(tuple(tail[-1]))
+                q[0] += max(-1, min(1, dx))
+                q[1] += max(-1, min(1, dy))
+
+            visited.add(tuple(rope[-1]))
     return len(visited)
 
 
