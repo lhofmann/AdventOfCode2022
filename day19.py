e7b5999636f08a8f1cbb9f275ffbbf534ac2d774
lhofmann <lhofmann@users.noreply.github.com>
2022-12-19 20:58:55 +0100

    Day 19

---
+++ day19.py
@@ -0,0 +1,111 @@
+from heapq import heappush, heappop
+import re
+
+DATA = open('day19.txt').read()
+
+
+def push(S, t, ore, clay, obsidian, geode, ore_robots,
+         clay_robots, obsidian_robots, geode_robots):
+    heuristic = t * geode_robots + t * (t - 1) // 2
+    heappush(
+        S,
+        (-(geode + heuristic),
+         t,
+         ore,
+         clay,
+         obsidian,
+         geode,
+         ore_robots,
+         clay_robots,
+         obsidian_robots,
+         geode_robots))
+
+
+def max_geodes(time, ore_ore, clay_ore, obsidian_ore,
+               obsidian_clay, geode_ore, geode_obsidian):
+    S = []
+    push(S, time, 0, 0, 0, 0, 1, 0, 0, 0)
+    visited = set()
+    while S:
+        state = heappop(S)
+        if state in visited:
+            continue
+        visited.add(state)
+        _, t, ore, clay, obsidian, geode, ore_robots, clay_robots, obsidian_robots, geode_robots = state
+        if t == 0:
+            return geode
+        if ore >= geode_ore and obsidian >= geode_obsidian:
+            push(S,
+                 t - 1,
+                 ore + ore_robots - geode_ore,
+                 clay + clay_robots,
+                 obsidian + obsidian_robots - geode_obsidian,
+                 geode + geode_robots,
+                 ore_robots,
+                 clay_robots,
+                 obsidian_robots,
+                 geode_robots + 1)
+            continue
+        if ore >= ore_ore and ore_robots < max(ore_ore, clay_ore, geode_ore):
+            push(S,
+                 t - 1,
+                 ore + ore_robots - ore_ore,
+                 clay + clay_robots,
+                 obsidian + obsidian_robots,
+                 geode + geode_robots,
+                 ore_robots + 1,
+                 clay_robots,
+                 obsidian_robots,
+                 geode_robots)
+        if ore >= clay_ore and clay_robots < obsidian_clay:
+            push(S,
+                 t - 1,
+                 ore + ore_robots - clay_ore,
+                 clay + clay_robots,
+                 obsidian + obsidian_robots,
+                 geode + geode_robots,
+                 ore_robots,
+                 clay_robots + 1,
+                 obsidian_robots,
+                 geode_robots)
+        if ore >= obsidian_ore and clay >= obsidian_clay and obsidian_robots < geode_obsidian:
+            push(S,
+                 t - 1,
+                 ore + ore_robots - obsidian_ore,
+                 clay + clay_robots - obsidian_clay,
+                 obsidian + obsidian_robots,
+                 geode + geode_robots,
+                 ore_robots,
+                 clay_robots,
+                 obsidian_robots + 1,
+                 geode_robots)
+        push(S,
+             t - 1,
+             ore + ore_robots,
+             clay + clay_robots,
+             obsidian + obsidian_robots,
+             geode + geode_robots,
+             ore_robots,
+             clay_robots,
+             obsidian_robots,
+             geode_robots)
+
+
+result = 0
+for blueprint in (tuple(map(int, re.findall(r'\d+', line)))
+                  for line in DATA.splitlines()):
+    geodes = max_geodes(24, *blueprint[1:])
+    result += blueprint[0] * geodes
+
+print(result)
+assert result == 1266
+
+
+result = 1
+for blueprint in (tuple(map(int, re.findall(r'\d+', line)))
+                  for line in DATA.splitlines()[:3]):
+    geodes = max_geodes(32, *blueprint[1:])
+    result *= geodes
+
+print(result)
+assert result == 5800
